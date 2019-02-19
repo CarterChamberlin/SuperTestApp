221a7d47172ad17997c8510c5102c27c5bd963e0
Carter Chamberlin <cdchamb3@asu.edu>
2019-02-19 10:19:08 -0700

    idk man

--- SuperTestApp/Program.cs
+++ SuperTestApp/Program.cs
@@ -10,6 +10,11 @@ namespace SuperTestApp
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("I love you");
+
+            Console.WriteLine("\n\n\nProgram has finished executing.\nPress any key to exit...");
+            Console.ReadKey();
         }
     }
 }
